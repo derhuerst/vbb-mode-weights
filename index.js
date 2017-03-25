31b48cf7c87cb8dcf22293578efe8f552d84f6dd
Jannis R <mail@jannisr.de>
2017-03-25 13:26:04 +0100

    implementation :clap:

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 'use strict'
 
-// todo
+const weights = {
+	  100:   1  // regional
+	, 102:   1  // regional
+	, 109:  .8  // suburban
+	, 400:  .7  // subway
+	, 700:  .25 // bus
+	, 3:    .25 // bus
+	, 900:  .35 // tram
+	, 1000: .6  // ferry
+}
+
+module.exports = weights
